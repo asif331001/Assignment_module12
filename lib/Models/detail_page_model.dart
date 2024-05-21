54c2bc49ff61943df8f5387168bcc1ecfa8c27bf
asif331001 <asif95331001@gmail.com>
2024-05-21 09:44:06 +0600

    first commit

---
+++ lib/Models/detail_page_model.dart
@@ -0,0 +1,8 @@
+import 'package:rest_api/Models/photo_model.dart';
+
+class DetailPageModel{
+  PhotoModel photoModel;
+  int index;
+
+  DetailPageModel(this.photoModel, this.index);
+}
---
+++ lib/Views/HomeScreen/home_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rest_api/Controllers/data_controller.dart';
+import 'package:rest_api/Models/photo_model.dart';
+import 'package:rest_api/Services/api_fetch.dart';
+import 'package:rest_api/Utils/constants.dart';
+import 'package:rest_api/Views/HomeScreen/home_screen_list_layout.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late DataController dataController;
+  List<PhotoModel> photoModel = [];
+
+  @override
+  void initState() {
+    dataController = DataController(ApiFetch());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(firstAppbarTitle),
+      ),
+      body: OrientationBuilder(
+        builder: (BuildContext context, Orientation orientation) {
+          return Container(
+            margin: const EdgeInsets.all(5.00),
+            child: Column(
+              children: [
+                FutureBuilder(
+                  future: dataController.loadPhotos(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      photoModel = snapshot.data!;
+                      return Visibility(
+                        visible: photoModel.isNotEmpty &&
+                            !dataController.isErrorOccurred,
+                        replacement: Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Center(
+                                child: SvgPicture.asset(
+                                  errorImage,
+                                  height: MediaQuery.of(context).size.height * 0.4,
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                              const SizedBox(height: 10,),
+                              const Text(errorMessage),
+                            ],
+                          ),
+                        ),
+                        child: Expanded(
+                          child: ListView.separated(
+                            itemCount: photoModel.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return HomeScreenListLayout(
+                                  orientation: orientation,
+                                  photoModel: photoModel,
+                                  index: index);
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) {
+                              return const SizedBox(
+                                height: 10,
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    }
+                    return const Center(
+                      child: LinearProgressIndicator(),
+                    );
+                  },
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
